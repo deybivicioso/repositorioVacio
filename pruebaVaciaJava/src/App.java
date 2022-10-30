7bf6588b0951ef2c8d8750729127aa69aa26e5e4
deybivicioso <darlexstv@gmail.com>
2022-10-30 13:26:27 -0400

    prueba1

---
+++ pruebaVaciaJava/src/App.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("prueba de un repositorio subido a Git");
+        System.out.println("ingrese el nombre del repositorio");
+        Scanner delivery=new Scanner(System.in);
+        String entrega=delivery.nextLine();
+        System.out.println(entrega);
+    }
+}
